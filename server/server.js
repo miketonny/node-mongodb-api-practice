8c6c9e8411225d74d21841b2025081e7f8f23eb2
miketonny <miketonny@users.noreply.github.com>
2018-07-07 18:16:58 +1200

    add patch updates

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const {ObjectID} = require('mongodb');
 const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const _ = require('lodash');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
 
 
 
@@ -71,6 +73,31 @@ app.delete('/todos/:id', (req, res) => {
          res.status(400).send();
      });
 });
+
+app.patch('/todos/:id', (req, res) =>{
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('Invalid id');
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(e =>{
+        res.status(400).send();
+    })
+});
+
 
 
 app.listen(port, () => {
