aaceb3184b9df20428a336a0473f8aaabb0edc42
miketonny <miketonny@users.noreply.github.com>
2018-07-08 15:55:41 +1200

    hash user pw before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var passowrd = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(passowrd, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+
+var hashedPasword = '$2a$10$fDi9OHDib/skOrswSIFSzOZNtfjN3XdvfCTdgRiGIYKyeOcmJ9gYG';
+bcrypt.compare(passowrd, hashedPasword, (err, res) => {
+    console.log(res);
+});
+
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // var message = 'i am user number 3';
 
