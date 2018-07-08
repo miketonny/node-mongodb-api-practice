f1f85c15def645b4c61101c9df80152f5fa4abb2
miketonny <miketonny@users.noreply.github.com>
2018-07-08 17:12:23 +1200

    tests for post login

--- server/models/user.js
+++ server/models/user.js
@@ -76,8 +76,11 @@ UserSchema.statics.findByCredentials = function(email, password){
         if(!user) return Promise.reject();
         return new Promise((resolve, reject) =>{
            bcrypt.compare(password, user.password, (err, res) =>{
-                if(err) reject();
-                resolve(user);
+                if(res) {
+                    resolve(user);
+                }else{
+                    reject();
+                }
            });
         });
     });
